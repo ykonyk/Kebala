a02a1b824c77070682c4c11c0206258053183e6f
ykonyk <66778509+ykonyk@users.noreply.github.com>
2021-05-27 12:43:37 +0200

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,8 +2,8 @@ package kabala_V2_4;
 
 public class Principal {
 	
-	//Cambiar los path de java para el correo
-	//Descargar Ansi escape on console en /help	/Eclipse marketplace
+	// Paso 1: Cambiar los path de java, con los jar de mail y activation adjuntos.
+	// Paso 2: Descargar 'Ansi Escape Console' desde el marcketplace de Eclipse.
 
 	public static void main(String[] args) throws Exception {
 		Operaciones.banner();
