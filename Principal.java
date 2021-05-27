5b78dbbee487a078a095bce83544eb69c17d24f6
ykonyk <66778509+ykonyk@users.noreply.github.com>
2021-05-27 12:36:07 +0200

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package kabala_V2_4;
+
+public class Principal {
+	
+	//Cambiar los path de java para el correo
+	//Descargar Ansi escape on console en /help	/Eclipse marketplace
+
+	public static void main(String[] args) throws Exception {
+		//Operaciones.banner();
+		Operaciones.seleccionComida();
+	}
+}
