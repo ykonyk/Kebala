ed49537d75ebb35539e5bfa288b3cbe3e78192f0
ykonyk <66778509+ykonyk@users.noreply.github.com>
2021-05-27 12:46:32 +0200

    Añadir clase Principal
    Desde donde se realiza la llamada de los bloques de operaciones y el banner.
    Con comentarios importantes.

--- Principal.java
+++ Principal.java
@@ -2,8 +2,8 @@ package kabala_V2_4;
 
 public class Principal {
 	
-	// Paso 1: Cambiar los path de java, con los jar de mail y activation adjuntos.
-	// Paso 2: Descargar 'Ansi Escape Console' desde el marcketplace de Eclipse.
+	// Paso 1: Cambiar los path de java, con los a rchivo jar de mail y activation adjuntos en el repositorio.
+	// Paso 2: Descargar 'Ansi Escape Console' desde el marcketplace de Eclipse, para ver coleres de consola.
 
 	public static void main(String[] args) throws Exception {
 		Operaciones.banner();
